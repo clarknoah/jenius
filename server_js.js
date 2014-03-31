78db614546a020aef42c6d6f1d383ea4c126ee43
clarknoah <noahbc08@gmail.com>
2014-03-31 13:49:08 -0400

    Update server_js.js

--- server_js.js
+++ server_js.js
@@ -1,3 +1,17 @@
 createSchemaProperty = function(payload){
   
   };
+
+
+exports.createSchemaRel = function(req, res){
+  var params = {};
+  params.srcId = req.body.srcId;
+  params.tgtId = req.body.tgtId;
+  params.relName = req.body.relName;
+  
+  query = "START srcNode = node({srcId}), tgtNode=node({tgtId})",
+          "MATCH (srcNode)<-[:source_schema_rel]-(rel:Schema_Relationship{",
+          "relationship_name:{relName},",
+          "created_on:timestamp()",
+          "})".join('\n');
+};
